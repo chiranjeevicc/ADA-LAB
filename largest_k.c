e04bd91ac755fe1304222d83e05430d72db11263
chiranjeevi <56463677+chiranjeevicc@users.noreply.github.com>
2020-02-20 12:44:03 +0530

    Add files via upload

---
+++ largest_k.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include<stdlib.h> 
+#include<time.h>
+  
+void swap(int *min, int *i) 
+{ 
+    int temp = *min; 
+    *min = *i; 
+    *i= temp; 
+} 
+  
+void selectionSort(int arr[10], int n) 
+{ 
+    int i, j, min_idx; 
+  
+   for (i = 0; i < n-1; i++) 
+    { 
+         min_idx = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+  
+        swap(&arr[min_idx], &arr[i]); 
+    } 
+} 
+
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]); 
+    printf("\n"); 
+} 
+ 
+int main() 
+{ 
+	clock_t start,end;
+	double time_taken;
+ 	int n,i;
+ 	int arr[10];
+ 	int max;
+ 	
+ 	printf("Enter the size of the array\n");
+ 	scanf("%d",&n);
+ 	printf("Enter the elements of the array\n");
+ 	for(i=0;i<n;i++){
+ 					scanf("%d",&arr[i]);
+ 				}
+ 	
+    start=clock();
+    selectionSort(arr, n); 
+    printf("Sorted array: \n"); 
+    printArray(arr, n); 
+    end=clock();
+    time_taken=((double)(end-start))/CLOCKS_PER_SEC;
+    printf("Time taken to comute the selection sort is %f\n",time_taken);
+    for(i=0;i<n;i++)
+    {
+         if(max<arr[i])
+         max=arr[i];
+     }
+   printf("the largest eliment of K is -->%d\n",max);                       
+    
+    return 0; 
+} 
